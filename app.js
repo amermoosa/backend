48fdae8c4ab9a3ac263f7d3172f9068d3cadc9f3
amermoosa <amermoosa@yahoo.com>
2024-03-18 10:51:10 +0300

    musa render

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 
 app.get("/test", (req, res) => {
-  res.status(200).send("hello welcome to musa market");
+  res.status(200).send("hello welcome to musa render market");
 });
 
 app.use("/api/v1/user", userRouter);
