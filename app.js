61a67e7f8bddc45e9c4e870d14f5d1ecb7e5ebb3
amermoosa <amermoosa@yahoo.com>
2024-03-22 01:02:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://musapazarlama.vercel.app/"],
+    origin: ["https://musapazarlama.vercel.app"],
     credentials: true,
   })
 );
