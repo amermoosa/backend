8954eb620b18c69a2a2c380614632347bf0f448f
amermoosa <amermoosa@yahoo.com>
2024-03-26 11:12:11 +0300

    Update authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -23,9 +23,9 @@ const sendJwtToken = (userId) => {
 
 const cookieOptions = {
   expires: new Date(Date.now() + 60 * 24 * 60 * 60 * 1000),
-  httpOnly: true,
-  sameSite: "lax",
-  secure: true,
+  httpOnly: false,
+  sameSite: "strict",
+  secure: false,
 };
 
 // Register a new
